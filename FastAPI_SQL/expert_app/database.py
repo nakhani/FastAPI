9e653ae9dfd483296b3cd5747aac6a2e2c81d60d
nakhani <najmehkhani.95@gmail.com>
2025-08-05 16:31:54 +0330

    add liara url

--- FastAPI_SQL/expert_app/database.py
+++ FastAPI_SQL/expert_app/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql://uni-admin:92422176@localhost:5432/university_db"
+#SQLALCHEMY_DATABASE_URL = "postgresql://root:lYoWgprTDAs31JXb6bfdYJeO@university-db:5432/postgres" # Liara Link
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
